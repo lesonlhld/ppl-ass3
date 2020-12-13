25e96ba39c6ed19a1e30264963f2cfb6e05e97bd
lesonlhld <lesonlhld@gmail.com>
2020-12-13 22:15:39 +0700

    updating files - 13/12/2020 22:15:39,43

--- src/test/CheckSuite.py
+++ src/test/CheckSuite.py
@@ -7,14 +7,26 @@ class CheckSuite(unittest.TestCase):
     def test_418(self):
         """Created automatically"""
         input = r"""
+        Var: x[10], y;
         Function: main
-        Parameter: x, y
         Body:
-            Var: z;
-            While (True) Do
-                z = main(1, main(x, True));
-            EndWhile.
-            Return y && z;
-        EndBody."""
+            Var: k;
+            k = -x[foo(x[0])];
+            x = f();
+        EndBody.
+        Function: foo
+        Parameter: y
+        Body:
+            Return y;
+        EndBody.
+        Function: f
+        Body:
+            Var: k[10];
+            If k[2] Then
+                Return k;
+            EndIf.
+            Return x;
+        EndBody.
+        """
         expect = str(TypeCannotBeInferred(Assign(Id("z"),CallExpr(Id("main"),[IntLiteral(1),CallExpr(Id("main"),[Id("x"),BooleanLiteral(True)])]))))
         self.assertTrue(TestChecker.test(input,expect,418))
